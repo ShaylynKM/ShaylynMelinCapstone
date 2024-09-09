dec2872167e60d64c5ec65ad79ea04989ab8331a
Shaylyn Melin <shaylynmelin@gmail.com>
2024-09-08 20:28:54 -0600

    Simple player movement done

--- Assets/_PROJECT/2_Scripts/PlayerMovement.cs
+++ Assets/_PROJECT/2_Scripts/PlayerMovement.cs
@@ -5,35 +5,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private MyInputActions _input = null;
+    private MyInputActions _input;
+
     private Vector2 _moveVector = Vector2.zero;
+
+    private Rigidbody2D _rb;
+
+    private SpriteRenderer _spriteRenderer;
+
+    private float _moveSpeed = 10f;
 
     private void Awake()
     {
         _input = new MyInputActions();
+
+        _rb = GetComponent<Rigidbody2D>();
+
+        _spriteRenderer = GetComponent<SpriteRenderer>();
     }
+
     private void OnEnable()
     {
         // Subscribe to our input action
+
         _input.Enable();
+
         _input.Player.Movement.performed += OnMovement;
+
         _input.Player.Movement.canceled += OnMovementCanceled;
     }
     private void OnDisable()
     {
         // Unsubscribe from our input action
+
         _input.Disable();
+
         _input.Player.Movement.performed -= OnMovement;
+
         _input.Player.Movement.canceled -= OnMovementCanceled;
     }
 
     private void OnMovement(InputAction.CallbackContext value) // This is what is providing the value to input
     {
-
+        _moveVector = value.ReadValue<Vector2>();
     }
 
     private void OnMovementCanceled(InputAction.CallbackContext value)
     {
+        _moveVector = Vector2.zero; // Stop the player from moving
+    }
 
+    private void FixedUpdate()
+    {
+        // Using FixedUpdate for physics interactions
+
+        _rb.velocity = _moveVector * _moveSpeed; // move player by multiplying the input by the speed
+    }
+
+    private void Update()
+    {
+        if (_moveVector.x > 0)
+        {
+            _spriteRenderer.flipX = false;
+        }
+        else if (_moveVector.x < 0)
+        {
+            _spriteRenderer.flipX = true;
+        }
     }
 }
