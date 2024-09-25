9db00dc91bbf908479fd9f1b0b583cab66ae4d01
ShaylynKM <shaylynmelin@gmail.com>
2024-09-25 14:50:27 -0600

    Separated the regular and battle player movement

---
+++ Assets/_PROJECT/2_Scripts/Player/PlayerBattleController.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerBattleController : PlayerController
+{
+    // Logic here goes if I want different movement for the battle
+}
