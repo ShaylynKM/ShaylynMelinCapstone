c2f7d3cdb4dd4e901d9f383e2f626e01ac100cee
Shaylyn Melin <shaylynmelin@gmail.com>
2024-09-28 17:28:30 -0600

    added required rb component for bullet destroyer

--- Assets/_PROJECT/2_Scripts/PrototypeBullet/BulletDestroyer.cs
+++ Assets/_PROJECT/2_Scripts/PrototypeBullet/BulletDestroyer.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(BoxCollider2D))]
+[RequireComponent(typeof(BoxCollider2D), typeof(Rigidbody2D))]
 public class BulletDestroyer : MonoBehaviour
 {
     // Component to tell the bullet if it should destroy itself
