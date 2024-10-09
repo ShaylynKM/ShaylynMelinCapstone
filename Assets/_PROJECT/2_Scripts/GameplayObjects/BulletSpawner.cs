ba4b88c9ed6c1b5773250b9b6b84b056c603f9ca
Shaylyn Melin <shaylynmelin@gmail.com>
2024-10-09 00:37:02 -0600

    Spawning logic cont.
    Configurable direction options

--- Assets/_PROJECT/2_Scripts/GameplayObjects/BulletSpawner.cs
+++ Assets/_PROJECT/2_Scripts/GameplayObjects/BulletSpawner.cs
@@ -1,23 +1,44 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class BulletSpawner : MonoBehaviour
 {
-    public Vector3 Direction { get; private set; }
-
     [SerializeField]
     private GameObject _bulletPrefab; // Bullets to be used with this spawner
 
-    private MoveStrategy _moveStrategy; // Used for assigning a direction
+    [SerializeField]
+    private GameObject _target; // Used if the bullet has a specific target to move towards
 
-    private void Awake()
+    // Used to configure the direction
+    [SerializeField]
+    private float _xDirection = 1f;
+    [SerializeField]
+    private float _yDirection = 0f;
+
+    // Should replace my individual instantiations with a pool of bullets
+
+    private void SpawnBulletWithDirection()
     {
-        _moveStrategy = _bulletPrefab.GetComponent<MoveStrategy>();
+        GameObject spawnedBullet = Instantiate(_bulletPrefab, this.transform.position, Quaternion.identity);
+
+        MoveStrategy moveStrategy = spawnedBullet.GetComponent<MoveStrategy>();
+
+        if(moveStrategy != null)
+        {
+            Vector3 direction = new Vector3(_xDirection, _yDirection, 0f);
+            moveStrategy.Initialize(this.transform.position, direction); // Initialize the bullet at this object's transform + facing the specified direction
+        }
     }
 
-    private void SetBulletDirection(Vector3 direction)
+    private void SpawnBulletWithTarget()
     {
-        
+        GameObject spawnedBullet = Instantiate(_bulletPrefab, this.transform.position, Quaternion.identity);
+
+        MoveStrategy moveStrategy = spawnedBullet.GetComponent<MoveStrategy>();
+
+        if (moveStrategy != null)
+        {
+            moveStrategy.Initialize(this.transform.position, _target); // Initialize the bullet at this object's transform + facing the target object
+        }
+
     }
 }
--- Assets/_PROJECT/2_Scripts/Patterns/Strategy/MoveStrategy/MoveStrategy.cs
+++ Assets/_PROJECT/2_Scripts/Patterns/Strategy/MoveStrategy/MoveStrategy.cs
@@ -1,40 +1,31 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public abstract class MoveStrategy : MonoBehaviour
 {
     [SerializeField]
-    protected Vector2 _direction;
+    protected Vector3 _direction;
 
     [SerializeField]
     protected float _speed;
 
-    [SerializeField]
-    protected BulletSpawner _spawner;
-
     public virtual void Initialize(Vector3 position, GameObject target)
     {
         // If the bullet has a specific target
-        transform.position = position;
-        _direction = (target.transform.position - transform.position).normalized;
+
+        transform.position = position; // Bullet's position
+        _direction = (target.transform.position - transform.position).normalized; // Bullet's direction (moving towards a target)
     }
      
     public virtual void Initialize(Vector3 position, Vector3 direction)
     {
-        // If the bullet just needs a position (direction specified by the spawner)
-        transform.position = position;
-        _direction = direction;
-    }
+        // If the bullet just needs a position (direction and position specified by the spawner)
 
-    public virtual void CalculateDirection(Vector3 direction)
-    {
-        // Act on the vector to decide which direction the object will move
+        transform.position = position; // Bullet's position
+        _direction = direction.normalized; // Bullet's direction
     }
 
     protected virtual void Update()
     {
-        //transform.rotation.eulerAngles = transform.LookAt(_direction);
-        transform.position += new Vector3(_direction.x, _direction.y, 0f) * _speed * Time.deltaTime; // Basic movement
+        transform.position += _direction * _speed * Time.deltaTime; // Basic movement
     }
 }
