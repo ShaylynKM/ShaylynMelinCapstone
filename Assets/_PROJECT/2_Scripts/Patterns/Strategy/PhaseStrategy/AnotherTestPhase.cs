aade838c4e6aa668e8a206bbdf4b21ede5712457
theseanzo <sean.zo.lynch@gmail.com>
2024-10-23 13:29:58 -0600

    Big changes
    Much strategy

--- Assets/_PROJECT/2_Scripts/Patterns/Strategy/PhaseStrategy/AnotherTestPhase.cs
+++ Assets/_PROJECT/2_Scripts/Patterns/Strategy/PhaseStrategy/AnotherTestPhase.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AnotherTestPhase : PhaseStrategy
+public class AnotherTestPhase : Phase
 {
     private void OnEnable()
     {
--- Assets/_PROJECT/2_Scripts/Patterns/Strategy/PhaseStrategy/TestPhase.cs
+++ Assets/_PROJECT/2_Scripts/Patterns/Strategy/PhaseStrategy/TestPhase.cs
@@ -2,10 +2,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class TestPhase : PhaseStrategy
+public class TestPhase : Phase
 {
-    private void OnEnable()
+    public override void StartPhase(BulletSpawner bulletSpawner)
     {
+        base.StartPhase(bulletSpawner);
         Debug.Log("Hi! I'm the first phase.");
         StartCoroutine(Wait());
     }
