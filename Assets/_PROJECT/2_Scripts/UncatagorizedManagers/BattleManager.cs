2dbefe425902f715067b9c7f8729992326523664
ShaylynKM <shaylynmelin@gmail.com>
2024-11-27 11:08:37 -0700

    Continued WIP, added function + tweaked comments

--- Assets/_PROJECT/2_Scripts/UncatagorizedManagers/BattleManager.cs
+++ Assets/_PROJECT/2_Scripts/UncatagorizedManagers/BattleManager.cs
@@ -4,14 +4,16 @@ using UnityEngine;
 using UnityEngine.Events;
 
 public class BattleManager : MonoBehaviour
-{
+{ 
+    ///  <summary>
     /// This class is responsible for linking the dialogue and battle phases together.
     /// Needs references to the dialogue objects
     /// Needs to be able to instantiate the bullet pattern objects for each phase
     /// Should be able to instantiate more than one pattern object during a phase
     /// Phases should be their own objects. All patterns needed exist inside that object, and they get set as active at specific times or in response to specific conditions (intervals)
     /// This script could have a list of the phase objects to activate??? Deciding if I want them all in the hierarchy at the start or instantiate them at runtime (it would be easier to set up events if they were all present in the scene at once
-    /// 
+    /// </summary>
+
     [SerializeField] private GameObject[] _phases; // Objects for each phase in the entire battle
     [SerializeField] private GameObject[] _dialogueObjects; // Objects containing the dialogue for each section
 
@@ -52,5 +54,10 @@ public class BattleManager : MonoBehaviour
                 Debug.LogError("Nothing in the phase array.");
             }
         }
+    }
+
+    public void BattleEnd()
+    {
+        // do stuff
     }
 }
